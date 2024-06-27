4d760e4474714a1ab5f678ce0ab2baeb1ebbc4a4
Dinesh Sahu <masterking008.com>
2024-06-27 18:59:45 +0530

    Carousel feature in booking

--- src/Components/BookAppointment.jsx
+++ src/Components/BookAppointment.jsx
@@ -1,41 +1,78 @@
 import { IonButton, setupIonicReact } from '@ionic/react';
 import './BookAppointment.css'
 setupIonicReact();
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function BookAppointment(props) {
 
-    return(
+    return (
         <>
+        <Carousel>
         <div className="appointment-body">
-            <div className="container">
-                <div className="description">
-                    <h2 className='heading'>Heading</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam recusandae corporis laudantium. Itaque minima eos laborum aperiam velit quod omnis rem sit praesentium dolorem repellat, totam modi nihil, maxime inventore!
-                    Nobis, corporis nemo.
-                    </p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, quibusdam amet provident tenetur enim corrupti suscipit nobis quam, fuga, saepe vel ipsum eum quo. Obcaecati quibusdam a nostrum officiis tenetur.
-                    Repellat officia consequuntur laboriosam id eum quidem doloribus,
-                    </p>
-                </div>
-                <div className="counselor-det">
-                    <div className="image"></div>
-                    <div className="creden">
-                        <ul>
-                            <li>Creden 1</li>
-                            <li>Creden 2</li>
-                            <li>Creden 3</li>
-                            <li>Creden 4</li>
-                        </ul>
+                <div className="container">
+                    <div className="description">
+                        <h2 className='heading'>Heading</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam recusandae corporis laudantium. Itaque minima eos laborum aperiam velit quod omnis rem sit praesentium dolorem repellat, totam modi nihil, maxime inventore!
+                            Nobis, corporis nemo.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, quibusdam amet provident tenetur enim corrupti suscipit nobis quam, fuga, saepe vel ipsum eum quo. Obcaecati quibusdam a nostrum officiis tenetur.
+                            Repellat officia consequuntur laboriosam id eum quidem doloribus,
+                        </p>
                     </div>
-                    <p className='appointment-det'>Price: Rs300</p>
-                    <p className='appointment-det'>Duration: 15 Minutes</p>
+                    <div className="counselor-det">
+                        <div className="image"></div>
+                        <div className="creden">
+                            <ul>
+                                <li>Creden 1</li>
+                                <li>Creden 2</li>
+                                <li>Creden 3</li>
+                                <li>Creden 4</li>
+                            </ul>
+                        </div>
+                        <p className='appointment-det'>Price: Rs300</p>
+                        <p className='appointment-det'>Duration: 15 Minutes</p>
+
+                        <IonButton className='book-btn'> Book An Appointment</IonButton>
+
+                    </div>
                 </div>
+
+            </div><div className="appointment-body">
+                <div className="container">
+                    <div className="description">
+                        <h2 className='heading'>Heading</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam recusandae corporis laudantium. Itaque minima eos laborum aperiam velit quod omnis rem sit praesentium dolorem repellat, totam modi nihil, maxime inventore!
+                            Nobis, corporis nemo.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, quibusdam amet provident tenetur enim corrupti suscipit nobis quam, fuga, saepe vel ipsum eum quo. Obcaecati quibusdam a nostrum officiis tenetur.
+                            Repellat officia consequuntur laboriosam id eum quidem doloribus,
+                        </p>
+                    </div>
+                    <div className="counselor-det">
+                        <div className="image"></div>
+                        <div className="creden">
+                            <ul>
+                                <li>Creden 1</li>
+                                <li>Creden 2</li>
+                                <li>Creden 3</li>
+                                <li>Creden 4</li>
+                            </ul>
+                        </div>
+                        <p className='appointment-det'>Price: Rs300</p>
+                        <p className='appointment-det'>Duration: 15 Minutes</p>
+
+                        <IonButton className='book-btn'> Book An Appointment</IonButton>
+
+                    </div>
+                </div>
+
             </div>
-            <div className="book">
-            <IonButton className='book-btn'> Book An Appointment</IonButton>
-            </div>
-        </div>
-        
+
+        </Carousel>
+            
+
         </>
     );
 }
--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Gallery from './gallery'
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function Testimonials() {
 
@@ -11,8 +12,44 @@ function Testimonials() {
                 <h2>Testimonials</h2>
             </div>
             
-            <Gallery/>
+            <Carousel>
+        <div className="box2">
+                    <div className="img2"></div>
+                    <div className="text">
 
+                        <div className="text1">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus optio. Dolor deserunt unde veniam quis architecto ducimus vel consequuntur, dicta illum exercitationem sequi perspiciatis dolorum quod eveniet suscipit. Eum.
+                            </p></div>
+                        <div className="text2"><p>Lorem ipsum dolor sit amet</p></div>
+                    </div>
+
+                </div>
+                <div className="box2">
+                    <div className="img2"></div>
+                    <div className="text">
+
+                        <div className="text1">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus optio. Dolor deserunt unde veniam quis architecto ducimus vel consequuntur, dicta illum exercitationem sequi perspiciatis dolorum quod eveniet suscipit. Eum.
+                            </p></div>
+                        <div className="text2"><p>Lorem ipsum dolor sit amet</p></div>
+                    </div>
+
+                </div>
+                <div className="box2">
+                    <div className="img2"></div>
+                    <div className="text">
+
+                        <div className="text1">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus optio. Dolor deserunt unde veniam quis architecto ducimus vel consequuntur, dicta illum exercitationem sequi perspiciatis dolorum quod eveniet suscipit. Eum.
+                            </p></div>
+                        <div className="text2"><p>Lorem ipsum dolor sit amet</p></div>
+                    </div>
+
+                </div>
+         </Carousel>
             
         </>
     )
